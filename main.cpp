631c9655ed74e425bc77b6a10465dd0831d99395
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2021-10-18 14:22:28 +0600

    user input filtration

--- main.cpp
+++ main.cpp
@@ -4,29 +4,58 @@ using namespace std;
 const int N = 3;
 char board[N][N];
 
-void boardFiller(int user_input, char sign)
+bool userInputConversion(int user_input, char sign)
 {
 
     if (user_input == 1)
-        board[2][0] = sign;
+    {
+        if(board[2][0] != 'X' && board[2][0] != 'O') board[2][0] = sign;
+        else return false;
+    }
     else if (user_input == 2)
-        board[2][1] = sign;
+    {
+        if(board[2][1] != 'X' && board[2][1] != 'O') board[2][1] = sign;
+        else return false;
+    }
     else if (user_input == 3)
-        board[2][2] = sign;
+    {
+        if(board[2][2] != 'X' && board[2][2] != 'O') board[2][2] = sign;
+        else return false;
+    }
 
     else if (user_input == 4)
-        board[1][0] = sign;
+    {
+        if(board[1][0] != 'X' && board[1][0] != 'O') board[1][0] = sign;
+        else return false;
+    }
     else if (user_input == 5)
-        board[1][1] = sign;
+    {
+        if(board[1][1] != 'X' && board[1][1] != 'O') board[1][1] = sign;
+        else return false;
+    }
     else if (user_input == 6)
-        board[1][2] = sign;
+    {
+        if(board[1][2] != 'X' && board[1][2] != 'O') board[1][2] = sign;
+        else return false;
+    }
 
     else if (user_input == 7)
-        board[0][0] = sign;
+    {
+        if(board[0][0] != 'X' && board[0][0] != 'O') board[0][0] = sign;
+        else return false;
+    }
     else if (user_input == 8)
-        board[0][1] = sign;
+    {
+        if(board[0][1] != 'X' && board[0][1] != 'O') board[0][1] = sign;
+        else return false;
+    }
     else if (user_input == 9)
-        board[0][2] = sign;
+    {
+        if(board[0][2] != 'X' && board[0][2] != 'O') board[0][2] = sign;
+        else return false;
+    }
+
+    return true;
 }
 
 void signPlacement(int row)
@@ -49,20 +78,10 @@ void signPlacement(int row)
         printf("     \n");
 }
 
-void printBoard()
+void showBoard()
 {
-    cout << "\n------------------TIC TAC TOE-----------------" << endl;
-
-    //testing output
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //     {
-    //         //cout << "(" << i << "," << j << ")" << " ";
-    //         cout << board[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
+    cout << "\n------------------TIC TAC TOE-----------------\n\n"
+         << endl;
 
     for (int row = 1; row < 10; row++)
     {
@@ -73,17 +92,17 @@ void printBoard()
 
         else if (row == 2)
         {
-         signPlacement(0);
+            signPlacement(0);
         }
 
         else if (row == 5)
         {
-         signPlacement(1);
+            signPlacement(1);
         }
 
         else if (row == 8)
         {
-         signPlacement(2);
+            signPlacement(2);
         }
 
         else
@@ -171,7 +190,7 @@ bool isMatched(int r, int c)
         return (a || b || d);
     }
 
-    // (0,1), (0,2), ...... [col]
+    // (0,1), (0,2), ...... [colomn]
     else if (r == 0)
     {
         //cout << "INSIDE COLUMN CHECKING.." << endl;
@@ -197,14 +216,13 @@ bool isMatched(int r, int c)
         a = rowChecking(r, c);
         return a;
     }
-
 }
 
 bool checkWinner()
 {
     bool checkCrossMatching;
 
-    for (int i = 0; i < N; i++) // becareful about indexing...N-1 or N-2??
+    for (int i = 0; i < N; i++) // becareful about indexing...N, N-1 or N-2??
     {
 
         if (i == 0)
@@ -268,26 +286,37 @@ int main()
 
             // check input
             if (user_input > 0 && user_input < 10)
-                break;
+            {
+
+                bool isVlaid;
+
+                // place input to corresponding cells
+
+                isVlaid = userInputConversion(user_input, sign);
+                if(isVlaid) break;
+                else {
+                    cout << "This place is already occupied by another player. Input Again!!\n\n";
+                }
+            }
+
             else
             {
                 cout << "Please Enter between 1 to 9" << endl;
             }
         }
 
-        // place input to corresponding cells
-        boardFiller(user_input, sign);
-
         // show the board
-        printBoard();
+        showBoard();
 
         // check winner
-        if (checkWinner())
+        if (checkWinner() == true)
         {
             if (turn % 2 == 0)
-                cout << ">>>>>> Player 1 is the Winner <<<<<<\n\n" << endl;
+                cout << ">>>>>> Player 1 (X) is the Winner <<<<<<\n\n"
+                     << endl;
             else
-                cout << ">>>>>> Player 2 is the Winner <<<<<<\n\n" << endl;
+                cout << ">>>>>> Player 2 (O)is the Winner <<<<<<\n\n"
+                     << endl;
             break;
         }
 
